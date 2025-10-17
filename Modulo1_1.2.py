0ced75581acff314bdae6a8154e1792cd0b05e59
David Gomes <davidoliveiraccs@gmail.com>
2025-10-17 22:20:58 +0100

    Add arithmetic and string operations in Modulo1_1.2.py

---
+++ Modulo1_1.2.py
@@ -0,0 +1,41 @@
+#Arithmetic operations
+a = 10
+b = 3
+#sum
+print(a+b)
+#subraction
+print(a-b)
+#division - the always become a float even if the value should not need a decimal case
+print(a/b)
+print(type(4/2))
+#multiplication
+print(a*b)
+#Potence
+print(a**b)
+#module
+print(a%b)
+#division of integers
+print(a//b)
+print(type(a//b))
+
+#operations with strings
+print("foo"*3)
+print('b'*3)
+#concatenation
+print("hello, " + str(5))
+print("hello, " + "world")
+
+#Comparisons
+#equal
+print(a==b)
+#bigger and bigger or equal
+print(a>b)
+print(a>=b)
+#smaller and bigger or equal
+print(a<b)
+print(a<=b)
+
+#strings
+print("hello" == "world")
+print("hello" < "world")
+print("hello" > "world")
